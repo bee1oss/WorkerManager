3ec4ee15f54e719583de5456238c6c2950e8f5b3
bee1oss <46090840+bee1oss@users.noreply.github.com>
2022-11-20 22:50:32 +0300

    Database and Login
    Database connection is adding. Login swing is ready and codding

--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,15 @@
+//Database
 /**
  *
  * @author begench
  */
+
 //ilk adimda Database baglantisi icin Database verilerinin tutuldugu yer
 public class Database {
+    public static final String username = "root"; //final diyerek hic bir yerde degistirilmesin
+    public static final String password = "";
+    public static final String db_name = "test";
+    public static final String host = "localhost";
+    public static final int port = 3306;
     
 }
--- src/EmployeeTransactions.java
+++ src/EmployeeTransactions.java
@@ -1,4 +1,10 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+
+//CalisanIslemleri
 /**
  *
  * @author begench
@@ -6,6 +12,46 @@
 //Burada ise calisan islemleri gerceklesecektir
 //bir de burada connection islemleri de gerceklesmesi gerekiyor + like CalisanIslemleri.java
 //Adding in library jarConnector for connection database
+//veritabani baglantisini yapabilmek icin Connection uzerinden bir obje uretecegiz ve bu obje uzerinden ilerleyecegiz
 public class EmployeeTransactions {
+    private Connection con = null;
     
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    
+    public Boolean logIn(int id , String password , String departmant){
+        
+        String query = "Select  * From workers where id = ? and password = ? and departmant = ?";
+        
+        try{
+            preparedStatement = con.prepareStatement(query);
+            
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, departmant);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+            
+            return rs.next();
+        }catch(SQLException ex){
+            Logger.getLogger(EmployeeTransactions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return false;
+    }
+    public EmployeeTransactions(){
+        //constructor for connection
+         String url = "jdbc:mysql://" + Database.host + ":" + Database.port + "/" + Database.db_name;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e){
+            System.out.println("Driver Bulunamadi...");
+        }
+        try{
+            con = DriverManager.getConnection(url,Database.username,Database.password);
+            System.out.println("sql.Baglanti Basarili...");
+        }
+        catch(SQLException e){
+            System.out.println("sql.Baglanti Basarisiz...");
+        }
+    }
 }
